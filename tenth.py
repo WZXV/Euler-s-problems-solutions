290c3794b20ecbb415814a18d203158c7fcbb873
WZXV <egor.pedan2016@yandex.ru>
2022-07-14 03:57:29 +0300

    add new algorithm

--- tenth.py
+++ tenth.py
@@ -1,15 +1,36 @@
-from math import sqrt, ceil
-
-
-def is_prime(n):
-    for i in range(2, ceil(sqrt(n)) + 1):
-        if n % i == 0:
+# Slow solution
+def is_prime(number):
+    for num in range(3, round(number ** 0.5) + 1):
+        if number % num == 0:
             return False
     return True
 
-res = 2
 
-for n in range(3, 2_000_000, 2git ):
-    if is_prime(n):
-        res += n
-print(res)
+def sum_primes_slow(n):
+    res = 2
+
+    for i in range(3, n, 2):
+        if is_prime(i):
+            res += i
+
+    return res
+
+
+print(sum_primes_slow(2_000_000))
+
+
+# Fast solution(Sieve of Eratosthenes)
+def sum_primes_fast(n):
+    res = 0
+    primes = [True] * n
+
+    for i in range(2, n):
+        if primes[i]:
+            res += i
+            for j in range(i ** 2, n, i):
+                primes[j] = False
+
+    return res
+
+
+print(sum_primes_fast(2_000_000))
